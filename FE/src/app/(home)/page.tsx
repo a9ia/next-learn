c361b5c4e058fd5e63db0ffe5598a6bc0770de00
a9ia <a9ia@qq.com>
2023-04-19 17:59:42 +0800

    delete main import

--- FE/src/app/(home)/page.tsx
+++ FE/src/app/(home)/page.tsx
@@ -2,7 +2,7 @@ import HomeSearch from "./(components)/HomeSearch"
 import HomeOverView from "./(components)/HomeOverView"
 import HomeDashBoard from "./(components)/HomeDashBoard/HomeDashBoard"
 import HomeFooter from "./(components)/HomeFooter"
-import "../../api/main"
+
 export default function Home() {
   return (
     <main className="max-w-full flex-grow">
